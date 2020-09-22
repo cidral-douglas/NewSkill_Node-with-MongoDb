d7c5fda4aaa5a74526e60f4b8b3633e90e3d7ee0
Douglas Cidral <doug.coding20@gmail.com>
2020-09-22 08:27:55 -0300

    Organizing

--- app.js
+++ app.js
@@ -3,14 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = 3000;
 
-const linkSchema = new mongoose.Schema({
-    title: {type:String, required:true},
-    description: String,
-    url: {type:String, required:true},
-    clicks: {type:Number, default:0}
-});
-
-const Link = mongoose.model('Link', linkSchema);
+const linkRoute = require('./routes/linkRoute');
 
 // let link = new Link({
 //     title: "progbr",
@@ -21,23 +14,10 @@ const Link = mongoose.model('Link', linkSchema);
 // link.save().then(doc=>{console.log(doc)}).catch(err=>{console.log(err)});
 
 mongoose.connect('mongodb://localhost/links', { useNewUrlParser: true, useUnifiedTopology: true}, (error,db) => {
-    console.log("Success");
-
-    app.get('/:title', async (req, res) => {
-
-        let title = req.params.title;
-        try {
-            let doc = await Link.findOne({title});
-            res.redirect(doc.url);
-        } catch (error) {
-            res.send("Houve um erro");
-        }
-    });
+    console.log("Success");  
 });
 
-app.get('/', (req, res)=>{
-    res.send("Hello World");
-});
+app.use('/', linkRoute);
 
 app.listen(PORT, ()=>{
     console.log("Running");
---
+++ controllers/linkController.js
@@ -0,0 +1,14 @@
+const Link = require('../models/Link');
+
+const redirect = async (req,res) => {
+
+    let title = req.params.title;
+    try {
+        let doc = await Link.findOne({title});
+        res.redirect(doc.url);
+    } catch (error) {
+        res.send("Houve um erro");
+    }
+};
+
+module.exports = { redirect }
