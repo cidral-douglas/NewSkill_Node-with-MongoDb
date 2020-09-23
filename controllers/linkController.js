db63f7ac638d7902fd41d8181d5cfd8cf17c1d98
Douglas Cidral <doug.coding20@gmail.com>
2020-09-23 10:10:35 -0300

    Adding the Edit links

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -17,7 +17,7 @@ const addLink = async (req, res) => {
 
     try {
         let doc = await link.save();
-        res.send("Link adicionado com sucesso");
+        res.redirect('/');
     } catch (error) {
         res.render('index', {error, body: req.body});
     }
@@ -40,10 +40,39 @@ const deleteLink = async (req,res) => {
 
     try {
         await Link.findByIdAndDelete(id);
-        res.send(id);
+        res.redirect('/');
     } catch (error) { 
         res.status(404).send(error);
     }
 }
 
-module.exports = { redirect, addLink, allLinks, deleteLink }
+const loadLink = async (req,res) => {
+    let id = req.params.id;
+
+    try {
+        let doc = await Link.findById(id);
+        res.render('edit', { error: false, body: doc });
+    } catch (error) { 
+        res.status(404).send(error);
+    }
+}
+
+const editLink = async (req, res) => {
+
+    let link = {};
+    link.title = req.body.title;
+    link.description = req.body.description;
+    link.url = req.body.url;
+
+    let id = req.params.id;
+    if (!id) { id = req.body.id;};
+
+    try {
+        let doc = await Link.updateOne({_id: id}, link)
+        res.redirect('/');
+    } catch (error) {
+        res.render('edit', {error, body: req.body});
+    }
+};
+
+module.exports = { redirect, addLink, allLinks, deleteLink, loadLink, editLink }
